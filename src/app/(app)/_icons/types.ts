7e9f6657f78bc63003685bca191208c4d8ce1386
absolutegravitas <124488796+absolutegravitas@users.noreply.github.com>
2024-05-16 08:59:06 +1000

    app icon

--- src/app/(app)/_icons/types.ts
+++
@@ -1,8 +0,0 @@
-export interface IconProps {
-  color?: string
-  size?: 'small' | 'medium' | 'large' | 'full' | 'default' | string
-  bold?: boolean
-  className?: string
-  rotation?: number
-  style?: React.CSSProperties
-}
--- src/app/(app)/_queries/users.ts
+++
@@ -1,4 +0,0 @@
-import configPromise from '@payload-config'
-import { getPayloadHMR } from '@payloadcms/next/utilities'
-
-const payload = await getPayloadHMR({ config: configPromise })
