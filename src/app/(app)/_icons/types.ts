5304b272ac399a3b48fec75e731983bd3a5370ca
absolutegravitas <124488796+absolutegravitas@users.noreply.github.com>
2024-05-16 15:24:25 +1000

    add fe graphics and icons

---
+++ src/app/(app)/_icons/types.ts
@@ -0,0 +1,8 @@
+export interface IconProps {
+  color?: string
+  size?: 'small' | 'medium' | 'large' | 'full' | 'default' | string
+  bold?: boolean
+  className?: string
+  rotation?: number
+  style?: React.CSSProperties
+}
