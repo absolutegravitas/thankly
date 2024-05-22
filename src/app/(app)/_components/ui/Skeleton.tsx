496ac10658b4ccb0e6e427228e05efc2c315e5cc
absolutegravitas <124488796+absolutegravitas@users.noreply.github.com>
2024-05-22 16:31:47 +1000

    shadcn ui components

---
+++ src/app/(app)/_components/ui/Skeleton.tsx
@@ -0,0 +1,13 @@
+import cn from 'clsx'
+import type { ComponentProps } from 'react'
+
+function Skeleton({ className, ...props }: ComponentProps<'div'>) {
+  return (
+    <div
+      className={cn('animate-pulse rounded-md bg-zinc-100 dark:bg-zinc-800', className)}
+      {...props}
+    />
+  )
+}
+
+export { Skeleton }
