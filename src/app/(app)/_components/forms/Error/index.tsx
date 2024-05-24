4611e05ca39db251b6fcc78da17cf6a89c2d7ddf
absolutegravitas <124488796+absolutegravitas@users.noreply.github.com>
2024-05-24 10:41:11 +1000

    add forms

---
+++ src/app/(app)/_components/forms/Error/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { Props } from './types'
+
+import classes from './index.module.scss'
+
+const Error: React.FC<Props> = props => {
+  const { showError, message, className } = props
+
+  if (showError) {
+    return <p className={[classes.error, className].filter(Boolean).join(' ')}>{message}</p>
+  }
+
+  return null
+}
+
+export default Error
