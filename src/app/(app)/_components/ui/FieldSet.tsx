167fb60c3edc4aa614aa24f5caceddda1f2447b8
absolutegravitas <124488796+absolutegravitas@users.noreply.github.com>
2024-05-28 23:26:32 +1000

    remove old ui components

--- src/app/(app)/_components/ui/FieldSet.tsx
+++
@@ -1,7 +0,0 @@
-import cn from 'clsx'
-import React from 'react'
-import type { ComponentProps } from 'react'
-
-export function Fieldset({ className, ...props }: ComponentProps<'div'>) {
-  return <div {...props} className={cn(className, 'space-y-2')} />
-}
