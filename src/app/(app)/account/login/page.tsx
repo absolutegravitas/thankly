1be5e6166aa35f3340a648b3b9572076c4f8031f
absolutegravitas <124488796+absolutegravitas@users.noreply.github.com>
2024-05-29 10:11:32 +1000

    login page

---
+++ src/app/(app)/account/login/page.tsx
@@ -0,0 +1,25 @@
+import { fetchMe } from '@app/_queries/fetchMe'
+import { Metadata } from 'next'
+import { redirect } from 'next/navigation'
+
+import { mergeOpenGraph } from '@/utilities/mergeOpenGraph'
+import { Login } from './page_client'
+
+export default async () => {
+  const { user } = await fetchMe()
+
+  if (user) {
+    redirect(`/cloud?warning=${encodeURIComponent('You are already logged in')}`)
+  }
+
+  return <Login />
+}
+
+export const metadata: Metadata = {
+  title: 'Login | Payload Cloud',
+  description: 'Login to Payload Cloud',
+  openGraph: mergeOpenGraph({
+    title: 'Login | Payload Cloud',
+    url: '/login',
+  }),
+}
