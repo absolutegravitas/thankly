e5c7ff151df27c992edd3c47227d2818f7a9ca26
absolutegravitas <124488796+absolutegravitas@users.noreply.github.com>
2024-06-07 12:24:11 +1000

    add staticText

---
+++ src/utilities/staticText.ts
@@ -0,0 +1,9 @@
+export const messages = {
+  inStock: `In stock and ready to ship`,
+  lowStock: `Hurry! Low Stock`,
+  outOfStock: `Sorry we're sold out!`,
+  removeProductBase: `This thankly is already in your cart. `,
+  removeProductExtra: `Go to the Cart to add your messages and send to as many people as you'd like easily.`,
+  removeProductWarning: `Removing this thankly will also remove all receivers. To change receivers and still send this thankly, go to the Cart.`,
+  shippingFreeMessage: `FREE Delivery for orders over $100.`,
+}
