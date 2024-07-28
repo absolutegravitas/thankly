76b1eae7216fe6f0c789b1b60ece8f0dd06ad321
absolutegravitas <124488796+absolutegravitas@users.noreply.github.com>
2024-07-28 12:12:22 +1000

    UPD ProductGrid cleaner UI

--- src/app/(app)/_blocks/ProductGrid/index.tsx
+++ src/app/(app)/_blocks/ProductGrid/index.tsx
@@ -1,44 +1,36 @@
-import React, { CSSProperties } from 'react'
-import { BlockWrapper, PaddingProps } from '@app/_components/BlockWrapper'
+/**
+ * @file index.tsx
+ * @module ProductGrid
+ * @description This module renders a grid of product cards on the ecommerce website.
+ * @overview
+ * The ProductGrid component is a React functional component that takes an array of products as a prop. 
+ * It renders a section element with a grid layout, where each grid item is a ProductCard component displaying
+ * details of a specific product. This component is responsible for displaying all available products in a
+ * visually appealing grid format, making it easy for users to browse and view different products.
+ */
+
+import React from 'react'
 import { Product } from '@payload-types'
 import { ProductCard } from '@app/_components/ProductCard'
 
-export type ProductGridProps = {
-  products: Product[]
-  padding?: PaddingProps
-}
+export type ProductGridProps = { products: Product[] }
 
 export const ProductGrid: React.FC<ProductGridProps> = ({ products }) => {
-  // console.log('productGrid', products)
-  const productLength = products?.length ?? 0
-  const hasProducts = Array.isArray(products) && productLength > 0
-  const excessLength = productLength > 4 ? 8 - productLength : 4 - productLength
-  const wrapperStyle: CSSProperties = {
-    '--excess-length-large': excessLength,
-    '--excess-length-mid': productLength % 2 === 0 ? 0 : 1,
-  } as CSSProperties
-
   return (
     <React.Fragment>
-      {hasProducts && (
-        <div className="relative px-4 py-8 sm:px-6 lg:px-8 #bg-gray-100 dark:bg-neutral-900">
-          <div
-            className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 sm:gap-8 relative"
-            style={wrapperStyle}
-          >
-            {products?.map((product, index) => {
-              return (
-                <div key={index} className="w-full">
-                  <ProductCard
-                    className="h-full relative  dark:bg-neutral-800 overflow-hidden transition-all duration-300 ease-in-out  hover:-translate-y-1"
-                    {...product}
-                  />
-                </div>
-              )
-            })}
-          </div>
+      <section
+        aria-labelledby="products-heading"
+        className="mx-auto max-w-2xl px-4 pb-16 pt-12 sm:px-6 sm:pb-24 sm:pt-16 lg:max-w-7xl lg:px-8"
+      >
+        <h2 id="products-heading" className="sr-only">
+          Products
+        </h2>
+        <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+          {products?.map((product, index) => {
+            return <ProductCard key={index} {...product} />
+          })}
         </div>
-      )}
+      </section>
     </React.Fragment>
   )
 }
