457543c9f314a22459d909d89403c07ef4d020e9
alecazador <alexanderbowes@gmail.com>
2024-08-15 23:57:10 +1000

    Updated auth buttons to return currently page and return after auth UI done.

--- src/app/(app)/_components/Auth/user-button.tsx
+++ src/app/(app)/_components/Auth/user-button.tsx
@@ -8,15 +8,14 @@ import {
   DropdownMenuTrigger,
 } from './ui/dropdown-menu'
 
-import { useSession } from 'next-auth/react'
+import { signIn, signOut, useSession } from 'next-auth/react'
 import Link from 'next/link'
 
 export default function UserButton() {
   const { status, data: session } = useSession()
 
   if (status == 'authenticated') {
-    console.log('user is authenticated!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-    console.log(session)
+    process.env.AUTH_VERBOSE ? console.log(session) : undefined
   }
 
   return (
@@ -36,12 +35,13 @@ export default function UserButton() {
             >
               {status === 'unauthenticated' && (
                 <DropdownMenuItem className="flex items-center justify-center p-2">
-                  <Link
-                    href="/api/auth/signin"
+                  <Button
+                    // href="/api/auth/signin"
+                    onClick={() => signIn(undefined, { callbackUrl: window.location.href })}
                     className="w-full text-center px-4 py-2 bg-gray-200 text-gray-700 font-medium rounded-md hover:bg-gray-300 transition-colors duration-300"
                   >
                     Sign In
-                  </Link>
+                  </Button>
                 </DropdownMenuItem>
               )}
               {status === 'authenticated' && (
@@ -55,12 +55,13 @@ export default function UserButton() {
                     </div>
                   </DropdownMenuLabel>
                   <DropdownMenuItem className="flex items-center justify-center p-2">
-                    <Link
-                      href="/api/auth/signout"
+                    <Button
+                      onClick={() => signOut({ callbackUrl: window.location.href })}
+                      // href="/api/auth/signout"
                       className="w-full text-center px-4 py-2 bg-gray-200 text-gray-700 font-medium rounded-md hover:bg-gray-300 transition-colors duration-300"
                     >
                       Sign Out
-                    </Link>
+                    </Button>
                   </DropdownMenuItem>
                 </>
               )}
