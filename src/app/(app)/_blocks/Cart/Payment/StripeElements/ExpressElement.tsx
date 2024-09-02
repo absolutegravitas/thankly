c096fcc06dcedfcecfa9eb7ce7986523b77e99f4
absolutegravitas <124488796+absolutegravitas@users.noreply.github.com>
2024-09-02 14:35:33 +1000

    REM temp comment Stripe for build complete

--- src/app/(app)/_blocks/Cart/Payment/StripeElements/ExpressElement.tsx
+++
@@ -1,7 +0,0 @@
-export const StripeExpressElement = () => {
-  return (
-    <div id="express-checkout-element" style={{ visibility: expressVisibility }}>
-      <ExpressCheckoutElement onConfirm={onExpressCheckoutConfirm} onReady={onReady} />
-    </div>
-  )
-}
