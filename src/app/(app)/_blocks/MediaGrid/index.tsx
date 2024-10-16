f13ca40b18ae0f17e27db3ad32a676cdb97247ff
alecazador <alexanderbowes@gmail.com>
2024-10-16 15:52:31 +1100

    fixed mediagrid to use tailwind grid

--- src/app/(app)/_blocks/MediaGrid/index.tsx
+++ src/app/(app)/_blocks/MediaGrid/index.tsx
@@ -12,30 +12,32 @@ export const MediaGrid = ({ mediaGridFields }: Props) => {
     return `1/${count}`
   }
 
-  const widthSettings = `
-    w-${countToTailwind(colsMobile)}
-    md:w-${countToTailwind(colsMedium)}
-    lg:w-${countToTailwind(colsLarge)}
-    xl:w-${countToTailwind(colsXLarge)}
-    2xl:w-${countToTailwind(cols2XLarge)}`
+  const columnSettings = `
+    grid-cols-${colsMobile}
+    md:grid-cols-${colsMedium}
+    lg:grid-cols-${colsLarge}
+    xl:grid-cols-${colsXLarge}
+    2xl:grid-cols-${cols2XLarge}`
 
   return (
     <div className="flex justify-center">
-      <div className="flex flex-grow flex-wrap p-4 pt-0 max-w-6xl">
-        {mediaGridFields.items?.map((item, index) => (
-          <div key={index} className={`flex flex-col ${widthSettings} px-3`}>
-            <div className="flex justify-center items-start">
-              <Image
-                className="object-cover"
-                src={item.image ? (item.image as Media).url : ''}
-                alt={item.image ? (item.image as Media).alt : ''}
-                width={1000}
-                height={1000}
-              />
+      <div className="w-full max-w-6xl p-4 pt-0">
+        <div className={`grid ${columnSettings} gap-4`}>
+          {mediaGridFields.items?.map((item, index) => (
+            <div key={index} className="flex flex-col">
+              <div className="flex justify-center items-start">
+                <Image
+                  className="object-cover w-full h-auto"
+                  src={item.image ? (item.image as Media).url : ''}
+                  alt={item.image ? (item.image as Media).alt : ''}
+                  width={1000}
+                  height={1000}
+                />
+              </div>
+              <div className="text-center text-xs md:text-sm mt-2">{item.text}</div>
             </div>
-            <div className="text-center text-xs md:text-sm">{item.text}</div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   )
